b7088d3bc9a323286c9bf024ee408e1ad19382a3
Roberto mejia <romejia1390@hotmail.com>
2016-07-06 10:04:52 -0500

    se actualizan archivos y agregan

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','pug');
+
+app.use(express.static('public'));
+
 app.get('/', function(req,res)
 {
-	res.send("Hola Mundo!!");
+	res.render('index');
+})
+
+app.get('/signup', function(req,res)
+{
+	res.render('index');
+})
+app.get('/signin', function(req,res)
+{
+	res.render('index');
 })
 
 
